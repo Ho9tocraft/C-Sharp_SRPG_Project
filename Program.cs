245a429259d5e2e7d989d582a79f1d3540e1581d
Ho9tocraft <ho9tocraft222@gmail.com>
2022-06-20 13:49:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibDLL;
+using ProjectSRPG2nd.data;
+
+using static DxLibDLL.DX;
+
+namespace ProjectSRPG2nd
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            ChangeWindowMode(TRUE);
+            if (DxLib_Init() == -1) return -1;
+            WaitKey();
+            DxLib_End();
+            return 0;
+        }
+    }
+}
---
+++ data/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSRPG2nd.data
+{
+    public class Unit
+    {
+        public string Unit_Name { get; protected set; }
+        public string Unit_NickName { get; protected set; }
+        public string Unit_DefineType { get; protected set; }
+    }
+
+    public class Unit_Effect
+    {
+
+    }
+}
